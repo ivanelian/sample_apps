de09304e75a28cbb821bbf15c64d244d3223feb4
Ivan <ivan@example.com>
2013-07-07 22:26:22 +0700

    Full Sign In & Out

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -14,7 +14,23 @@ describe "UserPages" do
         expect { click_button submit }.not_to change(User, :count)
       end
     end
+    
+    describe "after submission" do
+        before { click_button submit }
 
+        it { should have_selector('title', text: 'Sign Up') }
+        #it { should have_content('error') }
+    end
+    
+    #describe "after saving the user" do
+    #    before { click_button submit }
+    #    let(:user) { User.find_by_email('user@example.com') }
+
+    #    it { should have_selector('title', text: user.name) }
+    #    it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+    #    it {should have_link('Sign Out')}
+    #end
+    
     describe "with valid information" do
       before do
         fill_in "Name",         with: "Example User"
